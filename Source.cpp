299bf2fc204bceb0869f12a1a05c33327b424e40
void-daniel <51021466+pwrdaniel@users.noreply.github.com>
2022-06-16 14:23:45 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,11 @@
 #include <comdef.h>
 #include <Wbemidl.h>
 #include <intrin.h>
+#include <stdlib.h>
+#include <conio.h>
 # pragma comment(lib, "wbemuuid.lib")
+
+//github.com/pwrdaniel -> PC-Specifications-info repo.
 
 using namespace std;
 
@@ -52,133 +56,131 @@ void cpuID(unsigned i, unsigned regs[4]) {
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
     username();
   
 
-    HRESULT hres;
-    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
-    if (FAILED(hres)) return 0;
-    hres = CoInitializeSecurity(NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
-    if (FAILED(hres)) {
-        CoUninitialize();
-        return 0;
-    }
-    IWbemLocator* pLoc = NULL;
-    hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID*)&pLoc);
-    if (FAILED(hres)) {
-        CoUninitialize();
-        return 0;
-    }
-    IWbemServices* pSvc = NULL;
-    hres = pLoc->ConnectServer(_bstr_t(L"root\\CIMV2"), NULL, NULL, 0, NULL, 0, 0, &pSvc);
-    if (FAILED(hres)) {
-        pLoc->Release();
-        CoUninitialize();
-        return 0;
-    }
-    hres = CoSetProxyBlanket(pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE);
-    if (FAILED(hres)) {
+        HRESULT hres;
+        hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+        if (FAILED(hres)) return 0;
+        hres = CoInitializeSecurity(NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
+        if (FAILED(hres)) {
+            CoUninitialize();
+            return 0;
+        }
+        IWbemLocator* pLoc = NULL;
+        hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID*)&pLoc);
+        if (FAILED(hres)) {
+            CoUninitialize();
+            return 0;
+        }
+        IWbemServices* pSvc = NULL;
+        hres = pLoc->ConnectServer(_bstr_t(L"root\\CIMV2"), NULL, NULL, 0, NULL, 0, 0, &pSvc);
+        if (FAILED(hres)) {
+            pLoc->Release();
+            CoUninitialize();
+            return 0;
+        }
+        hres = CoSetProxyBlanket(pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE);
+        if (FAILED(hres)) {
+            pSvc->Release();
+            pLoc->Release();
+            CoUninitialize();
+            return 0;
+        }
+        IEnumWbemClassObject* pEnumerator = NULL;
+        hres = pSvc->ExecQuery(bstr_t("WQL"),
+            bstr_t("SELECT * FROM Win32_VideoController"),
+            WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
+        if (FAILED(hres)) {
+            pSvc->Release();
+            pLoc->Release();
+            CoUninitialize();
+            return 0;
+        }
+        IWbemClassObject* pclsObj;
+        ULONG uReturn = 0;
+        while (pEnumerator)
+        {
+            HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+            if (0 == uReturn)break;
+            VARIANT vtProp;
+            hr = pclsObj->Get(L"Caption", 0, &vtProp, 0, 0);
+            wcout << "gpu_name: " << vtProp.bstrVal << endl;
+            VariantClear(&vtProp);
+        }
+
+        int CPUInfo[4] = { -1 };
+        __cpuid(CPUInfo, 0x80000000);
+        unsigned int nExIds = CPUInfo[0];
+
+        // Get the information associated with each extended ID.
+        char CPUBrandString[0x40] = { 0 };
+        for (unsigned int i = 0x80000000; i <= nExIds; ++i)
+        {
+            __cpuid(CPUInfo, i);
+
+            // Interpret CPU brand string and cache information.
+            if (i == 0x80000002)
+            {
+                memcpy(CPUBrandString,
+                    CPUInfo,
+                    sizeof(CPUInfo));
+            }
+            else if (i == 0x80000003)
+            {
+                memcpy(CPUBrandString + 16,
+                    CPUInfo,
+                    sizeof(CPUInfo));
+            }
+            else if (i == 0x80000004)
+            {
+                memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
+            }
+        }
+
+        unsigned regs[4];
+
+        // Get vendor
+        char vendor[12];
+        cpuID(0, regs);
+        ((unsigned*)vendor)[0] = regs[1]; // EBX
+        ((unsigned*)vendor)[1] = regs[3]; // EDX
+        ((unsigned*)vendor)[2] = regs[2]; // ECX
+        string cpuVendor = string(vendor, 12);
+
+        // Get CPU features
+        cpuID(1, regs);
+        unsigned cpuFeatures = regs[3]; // EDX
+
+        // Logical core count per CPU
+        cpuID(1, regs);
+        unsigned logical = (regs[1] >> 16) & 0xff; // EBX[23:16]
+        unsigned cores = logical;
+
+        if (cpuVendor == "GenuineIntel") {
+            // Get DCP cache info
+            cpuID(4, regs);
+            cores = ((regs[0] >> 26) & 0x3f) + 1; // EAX[31:26] + 1
+
+        }
+        else if (cpuVendor == "AuthenticAMD") {
+            // Get NC: Number of CPU cores - 1
+            cpuID(0x80000008, regs);
+            cores = ((unsigned)(regs[2] & 0xff)) + 1; // ECX[7:0] + 1
+        }
+
+
+
+        cout << "cpu_name: " << CPUBrandString << endl;
+        cout << "cpu_cores: " << cores << " (# of cores+threads)" << endl;
+
+        cout << "\n" << "\n";
         pSvc->Release();
         pLoc->Release();
-        CoUninitialize();
-        return 0;
+        pEnumerator->Release();
+
+        cout << "Press any key to exit...";
+        cin.get(); // Makes it so the program closes when a key input is detected.
+        
     }
-    IEnumWbemClassObject* pEnumerator = NULL;
-    hres = pSvc->ExecQuery(bstr_t("WQL"),
-        bstr_t("SELECT * FROM Win32_VideoController"),
-        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
-    if (FAILED(hres)) {
-        pSvc->Release();
-        pLoc->Release();
-        CoUninitialize();
-        return 0;
-    }
-    IWbemClassObject* pclsObj;
-    ULONG uReturn = 0;
-    while (pEnumerator)
-    {
-        HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
-        if (0 == uReturn)break;
-        VARIANT vtProp;
-        hr = pclsObj->Get(L"Caption", 0, &vtProp, 0, 0);
-        wcout << "gpu_name: " << vtProp.bstrVal << endl;
-        VariantClear(&vtProp);
-    }
-
-    int CPUInfo[4] = { -1 };
-    __cpuid(CPUInfo, 0x80000000);
-    unsigned int nExIds = CPUInfo[0];
-
-    // Get the information associated with each extended ID.
-    char CPUBrandString[0x40] = { 0 };
-    for (unsigned int i = 0x80000000; i <= nExIds; ++i)
-    {
-        __cpuid(CPUInfo, i);
-
-        // Interpret CPU brand string and cache information.
-        if (i == 0x80000002)
-        {
-            memcpy(CPUBrandString,
-                CPUInfo,
-                sizeof(CPUInfo));
-        }
-        else if (i == 0x80000003)
-        {
-            memcpy(CPUBrandString + 16,
-                CPUInfo,
-                sizeof(CPUInfo));
-        }
-        else if (i == 0x80000004)
-        {
-            memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
-        }
-    }
-
-    unsigned regs[4];
-
-    // Get vendor
-    char vendor[12];
-    cpuID(0, regs);
-    ((unsigned*)vendor)[0] = regs[1]; // EBX
-    ((unsigned*)vendor)[1] = regs[3]; // EDX
-    ((unsigned*)vendor)[2] = regs[2]; // ECX
-    string cpuVendor = string(vendor, 12);
-
-    // Get CPU features
-    cpuID(1, regs);
-    unsigned cpuFeatures = regs[3]; // EDX
-
-    // Logical core count per CPU
-    cpuID(1, regs);
-    unsigned logical = (regs[1] >> 16) & 0xff; // EBX[23:16]
-    unsigned cores = logical;
-
-    if (cpuVendor == "GenuineIntel") {
-        // Get DCP cache info
-        cpuID(4, regs);
-        cores = ((regs[0] >> 26) & 0x3f) + 1; // EAX[31:26] + 1
-
-    }
-    else if (cpuVendor == "AuthenticAMD") {
-        // Get NC: Number of CPU cores - 1
-        cpuID(0x80000008, regs);
-        cores = ((unsigned)(regs[2] & 0xff)) + 1; // ECX[7:0] + 1
-    }
-
-
-
-    cout << "cpu_name: " << CPUBrandString << endl;
-    cout << "cpu_cores (# of threads): " << cores << endl;
-
-    cout << "\n" << "\n";
-    pSvc->Release();
-    pLoc->Release();
-    pEnumerator->Release();
-
-    int x;
-    cout << "Press any key to exit the program.";
-    cin >> x; // stops the program from exiting right away (press any key to exit).
-
-
-}
-
